2bd3a6f5423343508985451f708890f7bb86bd38
Denver Prophit Jr <3696477+inetbiz@users.noreply.github.com>
2022-05-28 13:08:26 -0400

    scss changes

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,1 @@
-// Bootstrap JS and CSS
-import 'bootstrap';
-
-import '../css/main.css';
-
-// Bootstrap Popover
-import './vendor/popover';
-
-// Bootstrap Tooltip
-import './vendor/tooltip';
+var bootstrap = require('bootstrap');
