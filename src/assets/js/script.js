654b6262a01c253b7372911246c76af956f0a68e
Denver Prophit Jr <3696477+inetbiz@users.noreply.github.com>
2022-06-02 22:46:24 -0400

    Update script.js

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -19,18 +19,6 @@ $(function () {
             }
         });
     }());
-    // --------------------------------------------------------------------
-    // jQuery for page scrolling feature - requires jQuery Easing plugin
-    // --------------------------------------------------------------------
-    (function () {
-        $('a.page-scroll').on('click', function (e) {
-            e.preventDefault();
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-        });
-    }());
     // -------------------------------------------------------------
     // mobile menu
     // -------------------------------------------------------------
