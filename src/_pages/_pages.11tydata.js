0b7ed478bdbbdb91cfb65e11ce3405ba2068620d
Denver Prophit Jr <3696477+inetbiz@users.noreply.github.com>
2022-06-14 19:25:17 -0400

    Create _pages.11tydata.js

---
+++ src/_pages/_pages.11tydata.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { toAbsoluteImageUrl } = require('../../_11ty/filters/filters');
+
+module.exports = {
+  layout: 'article',
+  permalink: (data) => `/articles/${data?.page?.fileSlug}/`,
+  isBlogPage: false,
+  isPost: true,
+  eleventyComputed: {
+    // For social sharing. Used in og:image and twitter:image. Absolute path to the post's thumbnail image.
+    // Thumbnails may be remote images (thumbnail.url) or local images (thumbnail as a string path, like ./images/thumbnail.png).
+    ogImage: async (data) => {
+      // inputPath will look like src/_page/yyyy-mm-dd-slug/ for a post located under src/_posts/yyyy-mm-dd-slug/index.md
+      const { dir: imgDir } = path.parse(data.page.inputPath);
+      // either a full URL for remote images or src/_posts/yyyy-mm-dd-slug/images/name.extension for local images
+      const src = data.thumbnail.url ?? path.join(imgDir, data.thumbnail);
+      // for remote images, just the URL; else, a root-relative path to the image (as seen in the img tag's src)
+      return toAbsoluteImageUrl(src);
+    },
+  },
+};
