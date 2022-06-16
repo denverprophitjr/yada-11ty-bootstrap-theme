5bb92ef70e52c617d99771c9092b05f733c4d6f0
Denver Prophit Jr <3696477+inetbiz@users.noreply.github.com>
2022-06-16 14:58:16 -0400

    JS syntax

--- webpack.config.js
+++ webpack.config.js
@@ -9,28 +9,25 @@ const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PATHS = {
-  src: path.join(__dirname, 'src')
-}
+  src: path.join(__dirname, 'src'),
+};
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
   stats: {
     colors: true,
-    preset: 'minimal'
+    preset: 'minimal',
   },
   performance: { hints: isDev ? false : 'warning' },
   // Eval does not work for css source maps
   // `All values enable source map generation except eval and false value.`
   // https://github.com/webpack-contrib/css-loader
   devtool: isDev ? 'cheap-module-source-map' : 'source-map',
-  entry: [
-    path.resolve(__dirname, 'src/assets/js/main.js'),
-    path.resolve(__dirname, 'src/assets/styles/main.scss')
-  ],
+  entry: [path.resolve(__dirname, 'src/assets/js/main.js'), path.resolve(__dirname, 'src/assets/styles/main.scss')],
   output: {
     filename: isDev ? '[name].js' : '[name].[contenthash].js',
     path: path.resolve(__dirname, '_site/assets'),
-    publicPath: '/assets/'
+    publicPath: '/assets/',
   },
   optimization: {
     splitChunks: {
@@ -39,34 +36,31 @@ module.exports = {
           name: 'styles',
           test: /\.css$/,
           chunks: 'all',
-          enforce: true
-        }
-      }
-    }
+          enforce: true,
+        },
+      },
+    },
   },
   plugins: [
     new WebpackManifestPlugin(),
     new MiniCssExtractPlugin({
-      filename: isDev ? '[name].css' : '[name].[contenthash].css'
+      filename: isDev ? '[name].css' : '[name].[contenthash].css',
     }),
-        new PurgecssPlugin({
-      paths: glob.sync(`${PATHS.src}/**/*`,  { nodir: true }),
+    new PurgecssPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
     }),
   ],
   ...(!isDev && {
     optimization: {
-      minimizer: [
-        new TerserPlugin(),
-        new CssMinimizerPlugin()
-      ]
-    }
+      minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
   }),
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
       },
       {
         test: /\.s?css/i,
@@ -75,31 +69,31 @@ module.exports = {
           'css-loader',
           {
             loader: 'postcss-loader',
-            options: { postcssOptions: { plugins: [PostCSSPresetEnv] } }
+            options: { postcssOptions: { plugins: [PostCSSPresetEnv] } },
           },
-          'sass-loader'
-        ]
+          'sass-loader',
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         type: 'asset',
         generator: {
-          filename: `images/${isDev ? '[name][ext]' : '[contenthash][ext]'}`
-        }
+          filename: `images/${isDev ? '[name][ext]' : '[contenthash][ext]'}`,
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-          filename: `fonts/${isDev ? '[name][ext]' : '[contenthash][ext]'}`
-        }
-      }
-    ]
+          filename: `fonts/${isDev ? '[name][ext]' : '[contenthash][ext]'}`,
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
       // Helpful alias for importing assets
-      assets: path.resolve(__dirname, 'src/assets')
-    }
-  }
+      assets: path.resolve(__dirname, 'src/assets'),
+    },
+  },
 };
